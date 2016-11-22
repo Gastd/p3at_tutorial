1975f636d3c143b56a9ef3c47ebcc8d9cb638168
Gastd <gabriel.araujo.5000@gmail.com>
2016-11-22 20:55:58 -0200

    Remove run.sh

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-export containerId=$(docker ps -l -q)
-xhost +local:`docker inspect --format='{{ .Config.Hostname }}' $containerId`
-docker run -it \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    rosjoy \
-    terminator
